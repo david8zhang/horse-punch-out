4d5bb739ff38066d98241143cdd821993a965b1f
Edward Zhang <edwardz123@gmail.com>
2022-11-03 22:50:55 -0700

    Add rudimentary enemy

--- src/core/Constants.ts
+++ src/core/Constants.ts
@@ -1,2 +1,8 @@
 export const WINDOW_WIDTH = 800
 export const WINDOW_HEIGHT = 600
+
+export enum Direction {
+  RIGHT = 'RIGHT',
+  LEFT = 'LEFT',
+  NONE = 'NONE',
+}
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,9 +1,11 @@
 import Phaser from 'phaser'
-import { WINDOW_HEIGHT, WINDOW_WIDTH } from '~/core/Constants'
+import { Direction, WINDOW_HEIGHT, WINDOW_WIDTH } from '~/core/Constants'
+import { Enemy } from '~/core/Enemy'
 import { Player } from '~/core/Player'
 
 export default class Game extends Phaser.Scene {
   public player!: Player
+  public enemy!: Enemy
 
   constructor() {
     super('game')
@@ -15,6 +17,22 @@ export default class Game extends Phaser.Scene {
         x: WINDOW_WIDTH / 2,
         y: WINDOW_HEIGHT - 50,
       },
+    })
+    this.enemy = new Enemy(this, {
+      position: {
+        x: WINDOW_WIDTH / 2,
+        y: WINDOW_HEIGHT - 400,
+      },
+    })
+    this.enemy.onPunch.push((punchDirection: Direction) => {
+      // TODO: put this logic somewhere else
+      if (this.player.currDodgeDirection == punchDirection) {
+        // player successfully dodged
+        console.log('damn, you doged it')
+      } else {
+        // got punched
+        console.log('u got punched')
+      }
     })
   }
 }
